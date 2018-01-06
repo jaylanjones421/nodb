cbec39f16f6b60f7f9b1a515dfa45af8488caa60
Jaylan Jones <jaylanjones421@gmail.com>
2018-01-06 09:57:22 -0600

    forgot a semicolon

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3002;
 const movieManager = require('./controllers/movieManager');
 const config = require('../config')
-const url = `https://api.themoviedb.org/3/discover/movie?api_key=${config}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${config}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 
 
 app.use(bodyParser.json());
